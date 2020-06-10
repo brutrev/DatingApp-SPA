71de2ff7addd4921b4d2d6720c797c0b3d7b06a4
brutrev <bru.trevz@gmail.com>
2020-06-10 02:35:34 -0300

    login stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      BookComponent
+      BookComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
